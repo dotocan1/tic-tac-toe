28e223f3670f6c2f674f10d18af107ba8d276ae7
Dominik Otocan <dominik.otocan1@gmail.com>
2021-04-06 21:53:35 +0200

    Current player works!

--- scripts.js
+++ scripts.js
@@ -1,24 +1,49 @@
-let Gameboard = (function () {
+const Gameboard = (function () {
     const grid = document.getElementById('grid-container');
     const gameboardArray = []
 
-    function saveElementToArray(){
-        
-    }
-    function renderTheGameboard() {
-        gameboardArray.forEach(function (element) {
-            gridItem.textContent = element;
-            grid.appendChild(gridItem);
-        })
+    function saveElementToArray(activePlayer) {
+        gameboardArray.push(activePlayer.symbol);
     }
 
-    return {renderTheGameboard};
+    function activePlayer(Player1, Player2) {
+        console.log(gameboardArray[0])
+        if (gameboardArray[gameboardArray.length - 1] === 'O' || gameboardArray[0] === undefined) {
+            return Player1;
+        } else {
+            return Player2;
+        }
+    }
+
+    function renderTheGameboard(Player1, Player2, target) {
+        //check if spot is already taken 
+
+        if (target.textContent === '') {
+            let currentPlayer = activePlayer(Player1, Player2);
+            saveElementToArray(currentPlayer);
+            target.textContent = currentPlayer.symbol;
+
+        }
+
+        else if (target.textContent != '') {
+            console.log('That spot is taken!')
+        }
+
+    }
+
+    return { renderTheGameboard };
 })();
 
-const grid = document.getElementById('grid-container');
-grid.addEventListener('click', function(e){
-    console.log(e.target);
-    e.target.textContent = 'X';
+const Player = function (name, symbol) {
+    return { name, symbol };
+}
 
+const grid = document.getElementById('grid-container');
+grid.addEventListener('click', function (e) {
+    let target = e.target;
+    Gameboard.renderTheGameboard(Player1, Player2, target)
 
 })
+
+const Player1 = Player('Dominik', 'X');
+const Player2 = Player('Luka', 'O');
